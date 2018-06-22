bee42b2cc398121fbcf1d5f68966270a24ffd7ed
Vincenzo Lucchese <vlucchese@altevie.com>
2018-06-22 15:46:51 +0200

    Classes and objects

---
+++ src/com/vince/javatutorial/classes_objects/objects/DisplayDeck.java
@@ -0,0 +1,15 @@
+package com.vince.javatutorial.classes_objects.objects;
+
+public class DisplayDeck {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        for (int suit = Card.DIAMONDS; suit <= Card.SPADES; suit++) {
+            for (int rank = Card.ACE; rank <= Card.KING; rank++) {
+                Card card = deck.getCard(suit, rank);
+                System.out.format("%s of %s%n",
+                        card.rankToString(card.getRank()),
+                        card.suitToString(card.getSuit()));
+            }
+        }
+    }
+}
